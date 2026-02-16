622bd815a7101ab48579486796d3514f9fb0eda8
Shubham Somse <shubhamsomse751@gmail.com>
2026-02-16 12:30:43 +0530

    Time: 1262 ms (48.25%) | Memory: 19.6 MB (45.15%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,25 +1,11 @@
 class Solution:
-    def lengthOfLIS(self, nums):
-        tails = [0] * len(nums)
-        size = 0
-
-        for x in nums:
-            i, j = 0, size
-            while i < j:
-                m = (i + j) // 2
-                if tails[m] < x:
-                    i = m + 1
-                else:
-                    j = m
-            tails[i] = x
-            if i == size:
-                size += 1
-
-        return size
-
-        #Greedy + Binary Search (Patience Sorting Technique)
-
-# Used to solve:
-
-# 👉 300 Longest Increasing Subsequence
-# in O(n log n) time instead of O(n²).
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        return max(dp)
