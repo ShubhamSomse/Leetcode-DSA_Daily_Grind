72172e374c9f392871b6c04450e2bcfeb7fd7d93
Shubham Somse <shubhamsomse751@gmail.com>
2026-03-27 12:42:12 +0530

    Time: 924 ms (56.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,14 @@
+SELECT
+    D.name AS Department,
+    E.name AS Employee,
+    E.salary AS Salary
+FROM (
+    SELECT
+        departmentId,
+        name,
+        salary,
+        DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
+    FROM Employee
+) E
+JOIN Department D ON E.departmentId = D.id
+WHERE E.salary_rank <= 3;
